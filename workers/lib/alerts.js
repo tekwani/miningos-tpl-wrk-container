e5a27d103be292456248a507dad815ea617897df
Hemant Tekwani <tekwani.hemant@gmail.com>
2026-05-02 01:59:32 +0530

    chore: facs version update

--- workers/lib/alerts.js
+++ workers/lib/alerts.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const libAlerts = require('miningos-tpl-wrk-thing/workers/lib/alerts')
+const libAlerts = require('@tetherto/miningos-tpl-wrk-thing/workers/lib/alerts')
 
 libAlerts.specs.container_default = libAlerts.specs.default
 
--- workers/lib/base.js
+++ workers/lib/base.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const BaseThing = require('miningos-tpl-wrk-thing/workers/lib/base')
+const BaseThing = require('@tetherto/miningos-tpl-wrk-thing/workers/lib/base')
 
 class BaseContainer extends BaseThing {
   constructor (opts = {}) {
